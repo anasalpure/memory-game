c6d13d2704467021aa8fe44fd313aafbcde77dae
anasbr <infomix82@gmail.com>
2018-05-28 11:49:46 -0700

    game works well without show win page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,84 @@
 /*
  * Create a list that holds all of your cards
+ * by default card have card class only
+ * if it match its pair it have  card and match classes
+ * if it open when click it have card and open  class  
  */
 
+var cards = document.querySelector("ul.deck").children;
 
+var stars= document.querySelector("ul.stars").children;
+var starsNumber=stars.length;
+var downRate=30000;  // 30 secound
+//number of matched images , end game when reach 8
+var matched=0;
+var counter=0;
+var movesCounter= document.querySelector("span.moves");
+var restartBitton=document.querySelector(".restart");
+var wait=false;
+//we need 2 varibles to store 2 cards that user select
+var first,secound;
+
+var arr=
+[
+    "fa-cube",
+    "fa-anchor",
+    "fa-diamond",
+    "fa-leaf",
+    "fa-bomb",
+    "fa-bolt",
+    "fa-bicycle",
+    "fa-paper-plane-o"
+];
+
+
+//initialize 
+init();
+
+
+for(let card of cards){
+    card.addEventListener("click",cardClicked);
+}
+
+//restart game
+restartBitton.addEventListener('click',restart);
+
+// stars rate will go down one star every 30 secound
+setTimeout(timerRate,downRate);
+
+
+
+
+function cardClicked(event){
+    // 2 cards unmatched => max wait for 500 milisecoind
+    if (wait) return;
+
+    let target =event.target;
+    let classes = target.classList;
+    /* by default card have card class only
+    * we'll show the image card to user
+    */
+    if(classes.length==1){
+        counter++;
+        movesCounter.textContent=counter;
+        classes.add("open");
+        ifUserSelectFirstCard(target);
+    }
+    /* card has card and match classes 
+    *  then we won't do anything
+    */
+    else if( classes.contains('match') ){
+        return 0;
+    }
+    /* card has card and open classes 
+    *  then we won't do anything
+    * and user reclick same card then w'll close this card  and put first =null
+    */
+    else if(classes.contains('open')){
+        classes.remove("open");
+        first =null;
+    }
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -11,6 +87,41 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+function init(){
+
+    let images=[...arr,...arr];
+    images=shuffle(images);
+    for(let i=0 ; i<images.length ; i++){
+        cards[i].children[0].classList.add(images[i]);
+    }
+
+    first=null;
+    secound=null;
+  
+}
+
+
+function restart(){
+    if(confirm("are you sure ")){
+        for(let card of cards){
+            if(card.classList.length>1){
+                card.classList.remove("open");
+                card.classList.remove("match");
+            }
+        }
+        counter=0;
+        movesCounter.textContent=counter;
+        //put 3 active stars
+        for(let star of stars){
+            star.children[0].classList.add("green");
+        }
+        
+        starsNumber=stars.length; 
+        setTimeout(timerRate,downRate);
+        init();
+    }
+}
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,6 +136,70 @@ function shuffle(array) {
     return array;
 }
 
+
+function compare (){
+    let f=first.children[0].className;
+    let s=secound.children[0].className;
+    if(f==s){
+        first.classList.add("match");
+        secound.classList.add("match");
+        first.classList.remove("open");
+        secound.classList.remove("open");
+        //check if user win
+        isPassed()
+    }
+    else{
+        wait=true;
+        setTimeout( function(){
+            if(first){
+                first.classList.remove("open");
+                first = null;
+            }
+            if(secound){
+                secound.classList.remove("open");
+                secound = null
+            }
+            wait=false;
+        }
+        ,500);
+   } 
+
+}
+
+function ifUserSelectFirstCard(card){
+    if(first == null){
+         first=card;
+    }
+    else{
+         secound=card;
+         secound.classList.add("open");
+    }
+    if(secound != null)
+        compare();
+}
+
+function isPassed(){
+  matched ++;
+  if(matched==8){
+      let currentStar=starsNumber;
+      let currentMoves=counter;
+      console.log(currentMoves+"success"+currentStar)
+
+      
+  }
+}
+
+
+
+function timerRate(){
+
+    if(starsNumber>0){
+        starsNumber--;
+        stars[starsNumber].children[0].classList.remove("green");
+        setTimeout(timerRate,downRate/1.3);
+    }
+    
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
